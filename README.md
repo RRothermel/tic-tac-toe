8d4eb1114db0148c90fc2e81f0fbcf324477ba04
RRothermel <79158870+RRothermel@users.noreply.github.com>
2021-03-09 11:16:05 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
-# hello-world
-My first example in Honors Software Design - 3/8/21
-I had Poptarts for breakfast today.
+### Tic Tac Toe with 1D List Project
+*by Ryan Rothermel*
+
+
+
+X is represented by 1
+O is represented by -1
+blank/empty square is represented by 0
+
+For example, the TTT board
+
+```
+X | X |
+---------
+X |   |
+---------
+O | O |
+```
+
+would be represented with the tic tac toe board entries 'peeled off' from top-down, left-right (row major, column minor) to fill the one-dimensional list variable `gameState` as in
+
+`gameState = [1, 1, 0, 1, 0, 0, 0, -1, -1]`
+
+or
+
+```
+gameState = [
+  1, 1, 0, # top row (positions 0, 1 & 2)
+  1, 0, 0, # middle row (positions 3, 4, & 5)
+  0, -1, -1  # bottom row (positions 6, 7, & 8)
+  ]```
---
+++ main.py
@@ -0,0 +1,81 @@
+# YOUR NAME HERE
+# Works Cited
+# None
+
+##### imports
+
+import gameFunctions
+import ioFunctions
+
+##### global constants
+
+
+##### global variables
+global gameState
+
+##### functions
+
+def main():
+  # main game logic
+
+  print("hello, game is started...")
+
+  # local constants
+
+  # general constants
+  SENTINEL = -1
+
+  # local variables
+
+  # game variables
+  gameInPlay = True
+  prompt = "Enter a value between 0 and 8 (" + str(SENTINEL) + " to quit): "
+  
+  debug = True
+  
+  gameState = [
+    0, 0, 0, 
+    0, 0, 0,
+    0, 0, 0
+  ]
+
+  numTurns = 0
+  while gameInPlay:
+    if debug:
+      print("debugging...")
+      gameFunctions.gameStateUI(gameState)
+
+    ioFunctions.boardDisplay(gameState)
+    numTurns += 1
+    userInput = ioFunctions.getIntegerInput(prompt)
+    # user wants to exit     
+    if userInput == SENTINEL:
+      break
+    elif not (userInput > -1 and userInput < 9):
+      print("invalid input " + str(userInput))
+    elif not gameState[userInput] == 0:
+      print("invalid input " + str(userInput) + ". That space is already taken")
+    else:
+      if numTurns % 2 == 1:
+        gameState[userInput] = 1
+        print("processing move by player O")
+      else:
+        gameState[userInput] = -1
+        print("processing move by player X")
+
+      if gameFunctions.isThereAWin(gameState):
+        print(gameState)
+        break
+
+      if gameFunctions.isThereADraw(gameState):
+        print("there is a draw")
+        break
+      else:
+        print("there is not a winner")
+  
+  print("goodbye, game is over")
+
+##### main
+
+if __name__ == "__main__":
+  main()
